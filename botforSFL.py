012645636696a256d600c980a4122b622eb73a1b
DROZD <drozdovlev3@gmail.com>
2018-12-15 06:54:11 +0300

    config upload

--- botforSFL.py
+++ botforSFL.py
@@ -5,6 +5,12 @@ from config import *
 
 def write_msg(user_id, text):
   vk_bot.method('messages.send', {'user_id': user_id, 'message': text, 'random_id': random.randint(0, 10000)})
+def write_msg_attach(user_id, text, att_url):
+  vk_bot.method('messages.send',
+             {'user_id': user_id,
+             'attachment': att_url,
+             'message': text,
+             'random_id': random.randint(0, 100000)})
 
 
 
@@ -25,8 +31,15 @@ while True:
   user_id = update[0][3]
   user_name = vk_bot.method('users.get', {'user_ids': user_id})
   write_msg(user_id, 'Привет, ' + (user_name[0]['first_name']))
-  print(str(user_name[0]['first_name']) + ' ' +
-  str(user_name[0]['last_name']) + ' написал(а) боту - ' + str(update[0][6]))
+  print(str(user_name[0]['first_name']) + ' ' + str(user_name[0]['last_name']) + ' написал(а) боту - ' + str(update[0][6]))
+  if 'фот' in update[0][6]:
+      write_msg_attach(user_id,
+                       'лови',
+                       'photo-155464693_456313827')
+  if 'муз' in update[0][6]:
+      write_msg_attach(user_id,
+                       'слушай',
+                       'какая-то музыка')
  ts = long_poll['ts']
-vvvvvvvv
+
 
