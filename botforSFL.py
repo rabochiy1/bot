8b1d42dce8e2075e7e23c24ad24966f86f46f5fa
DROZD <drozdovlev3@gmail.com>
2018-12-21 16:46:39 +0300

    config upload

--- botforSFL.py
+++ botforSFL.py
@@ -21,137 +21,148 @@ print("готов к использованию")
 # + str(long_poll))
 
 while True:
- long_poll = requests.get('https://{server}?act={act}&key={key}&ts={ts}&wait=5000000000000'.format(server=server,
+ try:
+  long_poll = requests.get('https://{server}?act={act}&key={key}&ts={ts}&wait=5000000000000'.format(server=server,
                                                                                         act='a_check',
                                                                                         key=key,
                                                                                         ts=ts)).json()
- update = long_poll['updates']
- if update[0][0] == 4:
- #print(update)
-  user_id = update[0][3]
-  user_name = vk_bot.method('users.get', {'user_ids': user_id})
-  print(str(user_name[0]['first_name']) + ' ' + str(user_name[0]['last_name']) + ' написал(а) боту - ' + str(update[0][6]))
-  if update[0][6]=='650':
+
+  update = long_poll['updates']
+  if update[0][0] == 4:
+  #print(update)
+   user_id = update[0][3]
+   user_name = vk_bot.method('users.get', {'user_ids': user_id})
+   print(str(user_name[0]['first_name']) + ' ' + str(user_name[0]['last_name']) + ' написал(а) боту - ' + str(update[0][6]))
+   if update[0][6]=='650':
      write_msg_attach(user_id,
                       'Затычка',
                       'photo261166398_456239824')
-  elif update[0][6]=='660ti':
+   elif update[0][6]=='660ti':
      write_msg_attach(user_id,
                       'Лучше 750ти на 2-3 фпс, особенно в 1920х1080',
                       'photo261166398_456239827')
-  if update[0][6]=='660':
+   if update[0][6]=='660':
       write_msg_attach(user_id,
                        'Как 750ти, но шина шире',
                        'photo261166398_456239833')
-  elif update[0][6]=='650ti':
+   elif update[0][6]=='650ti':
       write_msg_attach(user_id,
                            'Затычка, если повезет найдешь версию на 2 гига',
                            'photo261166398_456239826')
-  if '670' in update[0][6]:
+   if '670' in update[0][6]:
       write_msg_attach(user_id,
                        'Уделывает 760, но на 1 фазу питания меньше',
                        'photo261166398_456239832')
-  if '680' in update[0][6]:
+   if '680' in update[0][6]:
       write_msg_attach(user_id,
                        'Неплохая карта, хоть и жрёт 200вт',
                        'photo261166398_456239834')
-  if '750ti' in update[0][6]:
+   if '750ti' in update[0][6]:
       write_msg_attach(user_id,
                        'Первая карта на архитектуре Maxwell. Затычка, которая +- как GPU PS4',
                        'photo261166398_456239835')
-  if update[0][6]=='760':
+   if update[0][6]=='760':
       write_msg_attach(user_id,
                        'Неплохая видеокарта для 1600х900, есть версия на 1.5, 2, 3, 4 гига, отличаются шиной. Слабее 670 на 7-10%',
                        'photo261166398_456239836')
-  if '770' in update[0][6]:
+   if '770' in update[0][6]:
       write_msg_attach(user_id,
                        'Топ за свои деньги, почти как 1050ти, но жрёт 220вт. Есть версии на 2 и 4 гига',
                        'photo261166398_456239838')
-  elif update[0][6]=='760ti':
+   elif update[0][6]=='760ti':
       write_msg_attach(user_id,
                        'Редкий экземпляр, в России почти нет.',
                        'photo261166398_456239837')
-  if update[0][6]=='780':
+   if update[0][6]=='780':
       write_msg_attach(user_id,
                        'Прекрасная видеокарта, но жрет 250вт',
                        'photo261166398_456239839')
-  if 'R9 270' in update[0][6]:
+   if 'R9 270' in update[0][6]:
       write_msg_attach(user_id,
                        'Как 1050, но ценится майнерами. Полный аналог HD 7850 и R9 370, но имеет версию на 4 гига',
                        'photo261166398_456239841')
-  elif update[0][6]=='780ti':
+   elif update[0][6]=='780ti':
       write_msg_attach(user_id,
                        'Тоже ест 250вт, зато как 1060 на 3 гига',
                        'photo261166398_456239840')
-  if 'R9_270x' in update[0][6]:
+   if 'R9_270x' in update[0][6]:
       write_msg_attach(user_id,
                        'Аналог 7870 и R9 370x, чуть лучше 1050. Ценится майнерами',
                        'photo261166398_456239842')
-  if 'R9 280' in update[0][6]:
+   if 'R9 280' in update[0][6]:
       write_msg_attach(user_id,
                        'Аналог HD 7950 и R9 380, но у 380 версии на 2 и 4 гига. Ценится майнерами',
                        'photo261166398_456239843')
-  if 'R9_280x' in update[0][6]:
+   if 'R9_280x' in update[0][6]:
       write_msg_attach(user_id,
                        'Аналог HD 7970 и R9 380x, 380x отличается памятью',
                        'photo261166398_456239844')
-  if 'R9 290' in update[0][6]:
+   if 'R9 290' in update[0][6]:
       write_msg_attach(user_id,
                        'Первая карта от AMD, которая тянула 4к и тянет. Ест около 300вт, аналог R9 390',
                        'photo261166398_456239845')
-  if 'RX 550' in update[0][6]:
+   if 'RX 550' in update[0][6]:
       write_msg_attach(user_id,
-                       'Самая бесполезная карта. Имеет версии с 512 и 640 шейдерами, плюс 2 или 4 гига'
-                       'photo261166398_456239851')
-  if 'RX 560' in update[0][6]:
+                       'Самая бесполезная карта. Имеет версии с 512 и 640 шейдерами, плюс 2 или 4 гига',
+                       'photo261166398_456239851,photo261166398_456239852')
+   if 'RX 560' in update[0][6]:
       write_msg_attach(user_id,
                        'Перевыпуск RX 560 с небольшим разгоном и разблокированными шейдерами(можно было анлокнуть самому)',
                        'photo261166398_456239847')
-  if 'RX_560d' in update[0][6]:
+   if 'RX_560d' in update[0][6]:
       write_msg_attach(user_id,
                        'Полный аналог RX 460, есть версии на 2 и 4 гига',
                        'photo261166398_456239848')
-  if 'RX 570' in update[0][6]:
+   if 'RX 570' in update[0][6]:
       write_msg_attach(user_id,
                        'Не очень популярный аналог RX 470. Есть версии на 4 и 8 гигов',
                        'photo261166398_456239850')
-  if 'RX 580' in update[0][6]:
+   if 'RX 580' in update[0][6]:
       write_msg_attach(user_id,
                        'Популярный аналог RX 480. Имеет версии на 4 и 8 гигов',
                        'photo261166398_456239849')
-  if update[0][6]=='Vega 56':
+   if update[0][6]=='Vega 56':
       write_msg_attach(user_id,
                        'Видеокарта, имеющая память HBM2 и съедавшая около 250вт, но при этом проигрывает 1080',
                        'photo261166398_456239854')
-  if update[0][6] == 'Vega 64':
+   if update[0][6] == 'Vega 64':
      write_msg_attach(user_id,
                       'Имеет память HBM2 и жрёт около 300вт, но при этом проигрывает 1080ti',
                       'photo261166398_456239856')
-  if '950' in update[0][6]:
+   if '950' in update[0][6]:
       write_msg_attach(user_id,
                        'Наислабейший представитель 2-го прихода Максвелл.Затычка для 1080p, имеет версии на 2 и 4 гига',
                        'photo261166398_456239853')
-  if '960' in update[0][6]:
+   if '960' in update[0][6]:
       write_msg_attach(user_id,
                        'Видеокарта уровня 1050ti, иногда чуть слабее, есть версии на 2 и 4 гига. На картинке скрин моего видеоадптера',
                        'photo261166398_456239858')
-  if '970' in update[0][6]:
+   if '970' in update[0][6]:
       write_msg_attach(user_id,
                        'Карта с заводским косяком, из-за которого доступно 3.5 гига вместо 4. Производительность как 1063',
                        'photo261166398_456239859')
-  if update[0][6]=='1':
+   if '980' in update[0][6]:
+       write_msg_attach(user_id,
+                        'Карта, которая почти догоняет 1070',
+                        'photo261166398_456239866')
+   if '980ti' in update[0][6]:
+       write_msg_attach(user_id,
+                       'Карта, которая достойно тянет 4к и сейчас. Является обрубком Titan X, плюс жрёт 250вт',
+                       'photo261166398_456239866')
+   if update[0][6]=='1':
       write_msg(user_id,
             'GTX на архитектуре Kepler: 650, 650ti, 660, 660ti, 670, 680, 760, 760ti, 770, 780, 780ti ')
-  if update[0][6]=='2':
+   if update[0][6]=='2':
       write_msg(user_id,
                 'Карты на архитектуре GCN: R9 270, R9_270x, R9 280, R9_280x, R9 290, R9_290x, RX 550, RX 560, RX_560d, RX 570, RX 580, Vega 56, Vega 64')
-  if update[0][6]=='3':
+   if update[0][6]=='3':
       write_msg(user_id,
-                'Карты на архитектуре Максвелл: 750ti, 950, 960, 970')
-
-  else:
+                'Карты на архитектуре Максвелл: 750ti, 950, 960, 970, 980, 980ti')
+   else:
      write_msg(user_id,
      'Команды: 1, 2, 3; затем пиши названия карт')
- ts = long_poll['ts']
+  ts = long_poll['ts']
+ except KeyError:
+  vk_bot.method('messages.getLongPollServer', {'need_pts': 1, 'lp_version': 3})
 
 
